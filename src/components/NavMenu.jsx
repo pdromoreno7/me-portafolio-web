56337e29844fb063d163802cf1f8402425a97307
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-18 23:05:13 -0500

    Iniciamos creando NavMenu y llmando react router

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function NavMenu() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
