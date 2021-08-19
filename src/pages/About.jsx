81f5b96a03db90ac7b691ced20f2affa38d445a0
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-19 17:49:15 -0500

    Añadimos caprtea de pages con sus archivos, y terminamos rutas

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function About() {
+  return (
+    <div>
+      <h1>This is About Page</h1>
+    </div>
+  );
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Contact() {
+  return (
+    <div>
+      <h1>This is Contact Page</h1>
+    </div>
+  );
+}
