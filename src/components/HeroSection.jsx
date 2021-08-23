0f8ddd124b09a654eee6a593cb2ea36399f12a1b
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-23 14:46:50 -0500

    Terminamos estrutura html herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import HeroImg from '../assets/images/hero.png';
+import { Button } from './Button';
 import { Ptext } from './Ptext';
+import SocialMediaArrow from '../assets/images/social-media-arrow.svg';
+import ScrollDownArrow from '../assets/images/scroll-down-arrow.svg';
+
+// const HeroStyles = styled.div`
+//   .hero {
+//     height: 100vh;
+//     min-height: 1000px;
+//     width: 100%;
+//     text-align: center;
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     position: relative;
+//   }
+// `;
 
 export function HeroSection() {
   return (
@@ -18,6 +34,57 @@ export function HeroSection() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque autem
             quasi, pariatur, beatae totam maiores voluptate quwfe fwew
           </Ptext>
+          <Button btnLink="/projects" btnText="See my works" outline={false} />
+          <div className="hero__social">
+            <div className="hero__social__indicator">
+              <p>Follow</p>
+              <img src={SocialMediaArrow} alt="icon" />
+            </div>
+            <div className="hero__social__text">
+              <ul>
+                <li>
+                  <a
+                    href="http://facebook.com/webcifar"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    FB
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="http://twitter.com/webcifar"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    TW
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="http://isntagram.com/web_cifar"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    IG
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="http://webcifar.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    LI
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="hero__scrollDow">
+            <p>Scroll</p>
+            <img src={ScrollDownArrow} alt="ScrollDown Arrow" />
+          </div>
         </div>
       </div>
     </>
