0a8eefce97d0c1f96af0fb75ec6e1d71f36621ae
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-23 18:40:13 -0500

    añadi ProjecSection y ProyectItem con sus estilos y estructura

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { AboutSection } from '../components/AboutSection';
 import { HeroSection } from '../components/HeroSection';
+import { ProjectsSection } from '../components/ProjectsSection';
 
 export function Home() {
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <ProjectsSection />
     </>
   );
 }
