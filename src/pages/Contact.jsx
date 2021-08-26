e9b3148094c1772c33c8fbe552c32d57b8dcec38
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-25 21:42:33 -0500

    Se termino coneact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ContactSection from '../components/ContactSection';
 
 export function Contact() {
   return (
-    <div>
-      <h1>This is Contact Page</h1>
-    </div>
+    <>
+      <ContactSection />
+    </>
   );
 }
