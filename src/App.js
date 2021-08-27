eb115d80a0eae0bb782530fd815c114ea1b7df67
pdromoreno7 <pdro.moreno96@gmail.com>
2021-08-27 17:52:24 -0500

    Se añadio SmoothScrollbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import { NavMenu } from './components/NavMenu';
+import ScrollToTop from './components/SmoothScrollbar';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
 import { Home } from './pages/Home';
@@ -12,7 +13,7 @@ export default function App() {
     <>
       <Router>
         <NavMenu />
-
+        <ScrollToTop />
         <Switch>
           <Route path="/about">
             <About />
