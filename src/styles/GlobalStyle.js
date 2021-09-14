d21b16a3f5a75aae3788f5c8a89400fa333ac84c
pdromoreno7 <pdro.moreno96@gmail.com>
2021-09-14 15:47:45 -0500

    Se corrigio about item en mibile con erap

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,6 +7,7 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
   :root{
+    --HappyColor:#E3FF33 ;
     --dark-bg: #262626;
     --gray-1: #BCB4B4;
     --deep-dark: #1E1E1E;
