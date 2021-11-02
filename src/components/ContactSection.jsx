7611a040aedd6e1b9bb3221aae97f74a0b392c3b
pdromoreno7 <pdro.moreno96@gmail.com>
2021-11-01 23:50:11 -0500

    Se elimino carpeta de vscode con setting de prettier y de actualizo d¿nuvamente npm

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,11 +1,10 @@
-/* eslint-disable prettier/prettier */
-import React from 'react';
-import styled from 'styled-components';
-import { MdEmail } from 'react-icons/md';
-import { SiLinkedin } from 'react-icons/si';
-import ContactForm from './ContactForm';
-import ContactInfoItem from './ContactInfoItem';
-import SectionTitle from './SectionTitle';
+import React from "react";
+import styled from "styled-components";
+// import { MdEmail } from "react-icons/md";
+import { SiLinkedin } from "react-icons/si";
+import ContactForm from "./ContactForm";
+import ContactInfoItem from "./ContactInfoItem";
+import SectionTitle from "./SectionTitle";
 
 const ContactSectionStyle = styled.div`
   padding: 10rem 0;
@@ -13,12 +12,12 @@ const ContactSectionStyle = styled.div`
     display: flex;
     gap: 5rem;
     margin-top: 7rem;
-   justify-content: space-between;
+    justify-content: space-between;
     position: relative;
   }
   .contactSection__wrapper::after {
     position: absolute;
-    content: '';
+    content: "";
     width: 2px;
     height: 50%;
     background-color: var(--gray-1);
@@ -64,7 +63,6 @@ export default function ContactSection() {
               href="https://www.linkedin.com/in/pedro-moreno-alvarez/"
               target="_blank "
             >
-              
               <ContactInfoItem
                 icon={<SiLinkedin />}
                 text="Pedro Nel Moreno Alvarez"
@@ -73,9 +71,8 @@ export default function ContactSection() {
 
             {/* <ContactInfoItem icon={<MdEmail />} text="pdromorenodev@gmail.com" /> */}
             <a href="https://goo.gl/maps/pp3sHGmX7sRapLUt9" target="_blank ">
-                <ContactInfoItem text="Turbaco, Colombia" />
+              <ContactInfoItem text="Turbaco, Colombia" />
             </a>
-           
           </div>
           <div className="right">
             <ContactForm />
